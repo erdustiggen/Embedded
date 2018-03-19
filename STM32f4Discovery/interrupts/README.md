b8f41444983d7561684c825d7039a1b00069abde
Emil Braserud <erdustiggen@gmail.com>
2018-03-20 00:52:19 +0100

    External interrupt finished

--- STM32f4Discovery/interrupts/README.md
+++ STM32f4Discovery/interrupts/README.md
@@ -1,21 +1,14 @@
-NVIC can be used to give priority for interrupts. 
-I am using a struct defined in misc.h, which gives control of the IRQ, and you can change priorety. 
-Two different types of priority, Preemption and Sub. Preemtion used first, but if two has the same 
-preemtion priority, you can seperate them by using subpiority. Both take values between 0x00 and 0x0F.
-The lower the number, the higher the priority.
+In section one (GPIOs) we have 16 interrupt lines. They are line0 to line15 and they also represent pin number. 
+This means, PA0 is connected to Line0 and PA13 is connected to Line13.
 
-Lets say that UART1 is more important than UART2, then you can set the struct as following:
+You have to know that PB0 is also connected to Line0 and PC0 also and so on. This is for all pins on board.
+All Px0 (where x is GPIO name) pins are connected to Line0 and let’s say all Px3 are connected to Line3 on the Interrupt channel.
 
-NVIC_InitTypeDef NVIC_InitStruct;
- 
-NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
-NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
-NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
-NVIC_Init(&NVIC_InitStruct);
- 
-NVIC_InitStruct.NVIC_IRQChannel = USART2_IRQn;
-NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
-NVIC_InitStruct.NVIC_IRQChannelSubPriority = 1;
-NVIC_Init(&NVIC_InitStruct);
+IMPORTANT: You can not use two pins on one line simultaneously:
+
+PA0 and PB0 and PC0 and so on, are connected to Line0, so you can use only one pin at one time to handle interrupt from there.
+PA0 and PA5 are connected to different lines, they can be used at the same time.
+
+Each line can trigger an interrupt on rising, falling or rising_falling enge on signal.
+
+
--- STM32f4Discovery/interrupts/src/main.c
+++ STM32f4Discovery/interrupts/src/main.c
@@ -1,37 +1,146 @@
-// This won't work, but an example for how to use it.
-
-#include "stm32f4_discovery.h"
+#include "stm32f4xx.h"
 #include "stm32f4xx_conf.h"
+#include "stm32f4xx_exti.h"
+#include "stm32f4xx_syscfg.h"
+#include "stm32f4xx_gpio.h"
 #include "misc.h"
-#include"stm32f4xx_usart.h"
 
-NVIC_InitTypeDef NVIC_InitStruct;
- 
-NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
-NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
-NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
-NVIC_Init(&NVIC_InitStruct);
- 
-NVIC_InitStruct.NVIC_IRQChannel = USART2_IRQn;
-NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
-NVIC_InitStruct.NVIC_IRQChannelSubPriority = 1;
-NVIC_Init(&NVIC_InitStruct);
+// Function to configure pins to be interrupts
+void Configure_leds(void) {
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    GPIO_InitTypeDef gpio_struct;
+    gpio_struct.GPIO_Pin = GPIO_Pin_12;
+    gpio_struct.GPIO_Mode = GPIO_Mode_AF;
+    gpio_struct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOD, &gpio_struct);
+}
 
-//This will handle ALL interrupts which are enabled for USART1
-//so you have to find first which one was triggered
-void USART1_IRQHandler(void) {
-    //Check if interrupt was because data is received
-    if (USART_GetITStatus(USART1, USART_IT_RXNE)) {
-        //Do your stuff here
-        
-        //Clear interrupt flag
-        USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+void Configure_PD0(void) {
+
+    // Setting the variables to be used
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+
+    // Enable clock for GPIOD
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    // Enable clock for SYSCFG
+    RCC_AHB1PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+    // Setting the pin as input
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    /* Tell system that you will use PD0 for EXTI_Line0 */
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource0);
+
+    /* PD0 is connected to EXTI_Line0 */
+    EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+    /* Enable interrupt */
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    /* Interrupt mode */
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* Triggers on rising and falling edge */
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+    /* Add to EXTI */
+    EXTI_Init(&EXTI_InitStruct);
+
+    /* Add IRQ vector to NVIC */
+    /* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+    /* Set priority */
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+    /* Set sub priority */
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+    /* Enable interrupt */
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    /* Add to NVIC */
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+void Configure_PB12(void) {
+    /* Set variables used */
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+
+    /* Enable clock for GPIOB */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+    /* Enable clock for SYSCFG */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+    /* Set pin as input */
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Tell system that you will use PB12 for EXTI_Line12 */
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOB, EXTI_PinSource12);
+
+    /* PB12 is connected to EXTI_Line12 */
+    EXTI_InitStruct.EXTI_Line = EXTI_Line12;
+    /* Enable interrupt */
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    /* Interrupt mode */
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* Triggers on rising and falling edge */
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+    /* Add to EXTI */
+    EXTI_Init(&EXTI_InitStruct);
+
+    /* Add IRQ vector to NVIC */
+    /* PB12 is connected to EXTI_Line12, which has EXTI15_10_IRQn vector */
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI15_10_IRQn;
+    /* Set priority */
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+    /* Set sub priority */
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x01;
+    /* Enable interrupt */
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    /* Add to NVIC */
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+/* Set interrupt handlers */
+/* Handle PD0 interrupt */
+void EXTI0_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+    if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+        Configure_leds();
+
+        /* Clear interrupt flag */
+        EXTI_ClearITPendingBit(EXTI_Line0);
     }
 }
 
-int main() {
-// Do something
+/* Handle PB12 interrupt */
+void EXTI15_10_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+    if (EXTI_GetITStatus(EXTI_Line12) != RESET) {
+        /* Do your stuff when PB12 is changed */
 
+
+        /* Clear interrupt flag */
+        EXTI_ClearITPendingBit(EXTI_Line12);
+    }
+}
+
+int main(void) {
+    /* System init */
+    SystemInit();
+    /* Configure PD0 as interrupt */
+    Configure_PD0();
+    /* Configure PB12 as interrupt */
+    Configure_PB12();
+
+    while (1) {
+
+    }
 }
